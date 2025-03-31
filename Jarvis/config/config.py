e70d327464bbf5fc53f34e03d3fa46613f2a670c
Nigash <113447646+NIGASH333@users.noreply.github.com>
2025-03-31 20:16:28 +0530

    Update config.py

--- Jarvis/config/config.py
+++ Jarvis/config/config.py
@@ -1,5 +1,5 @@
 email = "nigashnigash0845@gmail.com"
-email_password = "Nigash333#"
+email_password = ""
 wolframalpha_id = "nigashnigash0845@gmail.com"
 weather_api_key = "49a56f01036b5d932b0d255f96bad940"
 
